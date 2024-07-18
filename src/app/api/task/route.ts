78249394dd89970c43e6745090d5c0eed7e2447c
zyllar68 <zyrilmancion@gmail.com>
2024-07-18 20:41:03 +0800

    CEHCK ERROR

--- src/app/api/task/route.ts
+++ src/app/api/task/route.ts
@@ -5,7 +5,6 @@ export async function POST(request: Request) {
 }
 
 export async function GET(request: Request) {
-  const tasks = await prisma.task.findMany();
-
+  // const tasks = await prisma.task.findMany();
   // return Response.json({ data: tasks }, { status: 200 });
 }
