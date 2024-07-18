1266e2d98a63d9f523cc1c95a158b3d9d86e0463
zyllar68 <zyrilmancion@gmail.com>
2024-07-18 20:13:25 +0800

    update

--- src/app/api/task/route.ts
+++ src/app/api/task/route.ts
@@ -1,11 +1,11 @@
-import prisma from '@/lib/prisma';
+// import prisma from '@/lib/prisma';
 
 export async function POST(request: Request) {
   return Response.json({ data: 'result' }, { status: 200 });
 }
 
-export async function GET(request: Request) {
-  const tasks = await prisma.task.findMany();
+// export async function GET(request: Request) {
+//   const tasks = await prisma.task.findMany();
 
-  return Response.json({ data: tasks }, { status: 200 });
-}
+//   return Response.json({ data: tasks }, { status: 200 });
+// }
--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,18 +1,18 @@
-import { PrismaClient } from '@prisma/client';
+// import { PrismaClient } from '@prisma/client';
 
-declare global {
-  var prisma: PrismaClient | undefined;
-}
+// declare global {
+//   var prisma: PrismaClient | undefined;
+// }
 
-let prisma: PrismaClient;
+// let prisma: PrismaClient;
 
-if (process.env.NODE_ENV === 'production') {
-  prisma = new PrismaClient();
-} else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
-  }
-  prisma = global.prisma;
-}
+// if (process.env.NODE_ENV === 'production') {
+//   prisma = new PrismaClient();
+// } else {
+//   if (!global.prisma) {
+//     global.prisma = new PrismaClient();
+//   }
+//   prisma = global.prisma;
+// }
 
-export default prisma;
+// export default prisma;
